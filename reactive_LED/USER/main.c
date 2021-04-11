ce3be12ada5dcad9717c01eae3555ed58d815994
kevin <644203270@qq.com>
2021-04-11 22:45:34 +0800

    Delete source insight file

--- reactive_LED/USER/main.c
+++ reactive_LED/USER/main.c
@@ -8,18 +8,20 @@ int main(void)
  
 	delay_init(168);		  //初始化延时函数
 	LED_Init();		        //初始化LED端口
-	
-  /**下面是通过直接操作库函数的方式实现IO控制**/	
-	
-//	while(1)
-//	{
-//	GPIO_ResetBits(GPIOF,GPIO_Pin_9);  //LED0对应引脚GPIOF.9拉低，亮  等同LED0=0;
-//	GPIO_SetBits(GPIOF,GPIO_Pin_10);   //LED1对应引脚GPIOF.10拉高，灭 等同LED1=1;
-//	delay_ms(500);  		   //延时300ms
-//	GPIO_SetBits(GPIOF,GPIO_Pin_9);	   //LED0对应引脚GPIOF.0拉高，灭  等同LED0=1;
-//	GPIO_ResetBits(GPIOF,GPIO_Pin_10); //LED1对应引脚GPIOF.10拉低，亮 等同LED1=0;
-//	delay_ms(500);                     //延时300ms
-//	}
+
+	while(1)
+	{
+		u8 read = GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_2);
+		u8 read1= GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_3);
+		if(read == 1)
+		{
+			GPIO_ResetBits(GPIOF, GPIO_Pin_10); //light up yellow led
+		}
+		else if(read1 == 1)
+		{
+			GPIO_SetBits(GPIOF, GPIO_Pin_10);
+		}
+	}
 }
 
 
